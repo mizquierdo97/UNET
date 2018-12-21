ce9790cb9944ab89e897ba7dd9c7e76c60ffcb6c
Guillermo Jesus Pinto Henriques <pintog96@gmail.com>
2018-12-21 14:14:53 +0100

    UI Scene

---
+++ UNET/Assets/newgamescene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class newgamescene : MonoBehaviour {
+
+   
+   public void New_Game_SC()
+    {
+       
+            SceneManager.LoadScene("SampleScene");
+        
+    }
+
+
+}
