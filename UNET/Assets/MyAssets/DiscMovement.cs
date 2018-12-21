f280ab9baad14019b63ece291c0a62cafd6e4c21
mizquierdo97 <mizquierdo97@gmail.com>
2018-12-21 13:30:03 +0100

    disc smooth movement

--- UNET/Assets/MyAssets/DiscMovement.cs
+++ UNET/Assets/MyAssets/DiscMovement.cs
@@ -1,20 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class DiscMovement : MonoBehaviour {
+public class DiscMovement : NetworkBehaviour
+{
 
     Rigidbody rigid;
     public float speed = 5;
-	// Use this for initialization
-	void Start () {
+    bool wait = false;
+    float disableTimer = 0.0f;
+    // Use this for initialization
+    Collider playerCollider;
+    SphereCollider discCollider;
+
+    [SyncVar(hook = "SyncVelocityChanged")]
+    public Vector3 velocity = Vector3.zero;
+
+    void Start () {
         rigid = GetComponent<Rigidbody>();
         rigid.velocity = transform.right * speed;
         //rigid.AddForce(transform.right*50);
+        discCollider = GetComponent<SphereCollider>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //transform.position += transform.right * Time.deltaTime;
-	}
+
+        if(hasAuthority)
+        {
+            SyncVelocityChanged(rigid.velocity);
+        }
+
+        if (wait)
+        {
+            disableTimer += Time.deltaTime;
+            Physics.IgnoreCollision(discCollider, playerCollider, true);
+            if (disableTimer >= 0.2f)
+            {
+                Physics.IgnoreCollision(discCollider, playerCollider, false);
+                wait = false;
+                disableTimer = 0.0f;
+            }
+        }
+
+        if(!hasAuthority)
+        {
+            transform.position = transform.position + velocity * Time.deltaTime;
+        }
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            playerCollider = collision.collider;
+            wait = true;
+        }           
+    }
+    void SyncVelocityChanged(Vector3 vel) { velocity = vel; }
 }
--- UNET/Assets/MyAssets/MovePlayer.cs
+++ UNET/Assets/MyAssets/MovePlayer.cs
@@ -12,9 +12,13 @@ public class MovePlayer : NetworkBehaviour {
     public float speed = 6;
     public PlayerManager playerManager;
     // Use this for initialization
+    int rotDirection = 1;
     Vector3 moveVec;
+    float startAngleY;
+    bool wait = false;
+    float waitTimer = 0.0f;
     void Start () {
-
+      
         playerManager = GameObject.Find("NetworkManager").GetComponent<PlayerManager>();
         playerManager.Connect(this);
 
@@ -23,49 +27,75 @@ public class MovePlayer : NetworkBehaviour {
             upCode = KeyCode.W;
             downCode = KeyCode.S;
             moveVec = new Vector3(0, 0, 1);
-            transform.position = new Vector3(-5.0f, -0.5f, 0.0f);
+            transform.position = new Vector3(-18.0f, -0.5f, 0.0f);
             transform.rotation = Quaternion.Euler(0,90,0);
         }
         if (playerCount == 2)
         {
             upCode = KeyCode.R;
             downCode = KeyCode.F;
-            moveVec = new Vector3(0, 0, -1);
-            transform.position = new Vector3(5.0f, -0.5f, 0.0f);
+            moveVec = new Vector3(0, 0, 1);
+            transform.position = new Vector3(18.0f, -0.5f, 0.0f);
             transform.rotation = Quaternion.Euler(0, -90, 0);
+            rotDirection = -1;
         }
         if (playerCount == 3)
         {
             upCode = KeyCode.Y;
             downCode = KeyCode.H;
             moveVec = new Vector3(1, 0, 0);
-            transform.position = new Vector3(0.0f, -0.5f, -5.0f);
+            transform.position = new Vector3(0.0f, -0.5f, -18.0f);
             transform.rotation = Quaternion.Euler(0, 0, 0);
+            rotDirection = -1;
         }
         if (playerCount == 4)
         {
             upCode = KeyCode.I;
             downCode = KeyCode.K;
-            moveVec = new Vector3(-1, 0, 0);
-            transform.position = new Vector3(0.0f, -0.5f, 5.0f);
+            moveVec = new Vector3(1, 0, 0);
+            transform.position = new Vector3(0.0f, -0.5f, 18.0f);
             transform.rotation = Quaternion.Euler(0, 180, 0);
         }
         trans = GetComponent<Transform>();
+        startAngleY = trans.rotation.eulerAngles.y;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (!isLocalPlayer) return;
+        if (!isLocalPlayer) return;        
+
+        //if (wait)
+        //{
+        //    waitTimer += Time.deltaTime;
+
+        //    if(waitTimer >= 0.2f)
+        //    {
+        //        wait = false;
+        //        waitTimer = 0.0f;
+        //    }
+        //    return;
+        //}
 
         if (Input.GetKey(upCode))
         {
             trans.position = trans.position + moveVec * Time.deltaTime * speed;
+            trans.rotation = Quaternion.Lerp(trans.rotation, Quaternion.Euler(new Vector3(0, startAngleY - rotDirection * 45, 0)), Time.deltaTime * 6);
         }
-        if (Input.GetKey(downCode))
+        else if (Input.GetKey(downCode))
         {
             trans.position = trans.position - moveVec * Time.deltaTime * speed;
+            trans.rotation = Quaternion.Lerp(trans.rotation, Quaternion.Euler(new Vector3(0, startAngleY + rotDirection * 45, 0)), Time.deltaTime * 6);
+        }
+        else
+        {
+            trans.rotation = Quaternion.Lerp(trans.rotation, Quaternion.Euler(new Vector3(0, startAngleY, 0)), Time.deltaTime * 10);
         }
 
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        wait = true;
     }
     public void Connected(int num)
     {
