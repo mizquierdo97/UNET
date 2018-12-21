aa8af588785e23ea5aec200d587d08f01d8a9cc1
Guillermo Jesus Pinto Henriques <pintog96@gmail.com>
2018-12-21 15:29:28 +0100

    script hud

---
+++ UNET/Assets/MyAssets/HUD.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class HUD : NetworkManager {
+
+    public void StartupHost()
+    {
+        SetPort();
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void JoinGame()
+    {
+        SetIPAddress();
+        SetPort();
+        NetworkManager.singleton.StartClient();
+    }
+
+    void SetIPAddress()
+    {
+        
+         string IPAddress= GameObject.Find("InputFieldText").GetComponent<Text>().text;
+        NetworkManager.singleton.networkAddress = IPAddress;
+            }
+
+    void SetPort()
+    {
+        NetworkManager.singleton.networkPort = 7777;
+    }
+}
